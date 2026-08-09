2a38710024aad909989a41b59f66f278dd26d51d
Edson Candido <admin@MacBook-Pro-de-Edson.local>
2026-08-09 12:55:21 -0300

    initial commit

---
+++ apps/server/src/index.ts
@@ -0,0 +1,39 @@
+import { createContext } from "@default-full-app/api/context";
+import { appRouter } from "@default-full-app/api/routers/index";
+import { auth } from "@default-full-app/auth";
+import { env } from "@default-full-app/env/server";
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
+import { toNodeHandler } from "better-auth/node";
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: env.CORS_ORIGIN,
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
+
+app.all("/api/auth{/*path}", toNodeHandler(auth));
+
+app.use(
+  "/trpc",
+  createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  }),
+);
+
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.status(200).send("OK");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
